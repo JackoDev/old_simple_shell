8c643fa8c725338099cde97f8ce047cb436d7808
Skillhh <ortegon.giovanny@hotmail.com>
2019-08-22 22:02:29 -0500

    [ADD] adding files .c

--- holberton.h
+++ holberton.h
@@ -6,10 +6,13 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <signal.h>
 
 int count_args(char **args);
 char *_strcpy(char *dest, const char *src);
 size_t _strlen(const char *s);
-
+int _strcmp(char *s1, char *s2);
+void print(const char str[], int len);
 #endif /** HOLBERTON_H*/

65752e4fbbbd8f65df3f1b4fe74c271c4af74dbc
Skillhh <ortegon.giovanny@hotmail.com>
2019-08-25 17:55:48 -0500

    [UPDATE] holberton.h

--- holberton.h
+++ holberton.h
@@ -9,6 +9,11 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <signal.h>
+#include <fcntl.h>
+
+#define CMD_SIZE 1024
+
+extern char **environ;
 
 int count_args(char **args);
 char *_strcpy(char *dest, const char *src);
@@ -16,5 +21,6 @@ size_t _strlen(const char *s);
 int _strcmp(char *s1, char *s2);
 int _putchar(char c);
 void _puts(char *str);
+void args_null(char **args, int height);
 
 #endif /** HOLBERTON_H*/

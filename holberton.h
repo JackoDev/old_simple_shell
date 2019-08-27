0cb25e95f8e169f5bac67c5b89b35fedcaa17150
untaljacko <untaljacko@gmail.com>
2019-08-27 14:10:05 -0500

    [ADD] _strcat.c and include it prototype inside holberton.h

--- holberton.h
+++ holberton.h
@@ -27,6 +27,7 @@ int count_args(char **args);
 char *_strcpy(char *dest, const char *src);
 size_t _strlen(const char *s);
 int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
 int _putchar(char c);
 void _puts(char *str);
 void args_null(char **args, int height);

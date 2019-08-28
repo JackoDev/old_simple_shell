0e38ff52a77a15d7ff2309237c43ae28faf93ebf
Skillhh <ortegon.giovanny@hotmail.com>
2019-08-28 13:00:21 -0500

    [UPDATE] prompt new

--- print_sing.c
+++ print_sing.c
@@ -5,10 +5,6 @@
  */
 void print_sign(void)
 {
-	char curr_path[512];
-
-	_puts("\33[1;31m[#]cisfun@holberton\33[0m:");
-	_puts("\33[1;34m");
-	_puts(getcwd(curr_path, -1));
-	_puts("\33[0m$ ");
+	if (isatty(STDIN_FILENO))
+		_puts("$ ");
 }
